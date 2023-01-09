fdcde0bfae83c29b84ba6efaf0e5fa13a087c7e5
Duy Le <dle2@oberlin.edu>
2023-01-09 10:07:04 -0500

    2a

---
+++ part2/course-info/src/components/Course.js
@@ -0,0 +1,20 @@
+const Header = ({course}) => <h3>{course}</h3>
+
+const Part = ({part}) => <p>{part.name} {part.exercises}</p>
+
+const Content = ({parts}) => ( parts.map((part, id) => (<Part key={id} part={part} />)) )
+
+const Total = ({parts}) => {
+  let sum = parts.reduce((a, b) => (a + b.exercises), 0)
+  return <strong>Totaling {sum} exercises</strong>
+}
+
+const Course = ({course}) => {
+  return (<>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+  </>)
+}
+
+export default Course
