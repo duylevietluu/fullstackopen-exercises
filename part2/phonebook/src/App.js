df4b2cc93f016c95515e3de0dabf32361e2dc7da
Duy Le <dle2@oberlin.edu>
2023-01-09 20:53:34 -0500

    2c (no *)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
- 
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState('')
   const [filterName, setFilterName] = useState('')
@@ -32,6 +32,16 @@ const App = () => {
   }
 
   const nameFilter = (person) => (person.name.toLowerCase().indexOf(filterName.toLowerCase()) !== -1)
+
+  // fetch data
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }
+  useEffect(hook, [])
 
   return (
     <div>
